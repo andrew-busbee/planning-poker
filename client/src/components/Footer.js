e3c5cb4165a7d36dae6ae9cbd98dd4c557718d7e
Andrew Busbee <21376025+andrew-busbee@users.noreply.github.com>
2025-09-13 01:46:05 -0500

    Release bump

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -27,12 +27,12 @@ const Footer = () => {
         <div className="footer-center">
           <div className="app-title">
             Planning Poker <a 
-              href="https://github.com/andrew-busbee/planning-poker/releases/tag/untagged-810af2974c72c551af64" 
+              href="https://github.com/andrew-busbee/planning-poker/releases/tag/v1.0.0" 
               target="_blank" 
               rel="noopener noreferrer"
               className="version-link"
             >
-              v1.0.1
+              v1.0.0
             </a>
           </div>
         </div>
